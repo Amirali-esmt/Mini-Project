e7420c423f9e83e80193c29c4195174c6eb0bfc5
Amirali Rad <amirali.rad@esmt.org>
2025-11-20 14:54:49 +0100

    Ali

--- mini-project.py
+++ mini-project.py
@@ -3,6 +3,6 @@ print("Please write your name here\n")
 
 #Each teammate should enter their name individually and push the update to GitHub
 print("Amirali")
-print("teammate2")
+print("ali")
 print("teammate3")
 print("teammate4")
