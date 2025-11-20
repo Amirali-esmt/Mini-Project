96803612ac09da53172d1dec0b5c9e0fd74bca0c
Amirali Rad <amirali.rad@esmt.org>
2025-11-20 14:22:23 +0100

    Initial commit

---
+++ mini-project.py
@@ -0,0 +1,13 @@
+print("Welcome to MAAI Workshop")
+print("Please write your name here\n")
+
+teammate1 = input("Teammate 1: Enter your name: ")
+teammate2 = input("Teammate 2: Enter your name: ")
+teammate3 = input("Teammate 3: Enter your name: ")
+teammate4 = input("Teammate 4: Enter your name: ")
+
+print("\nThank you! Here are the names you entered:")
+print("Teammate 1:", teammate1)
+print("Teammate 2:", teammate2)
+print("Teammate 3:", teammate3)
+print("Teammate 4:", teammate4)
